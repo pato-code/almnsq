db1ff4ca5c7ed94c708c7973b82f97355f4ce1bb
unknown <akame.pato@gmail.com>
2018-11-24 17:46:24 +0200

    change design

--- resources/views/Neighborhood/create.blade.php
+++ resources/views/Neighborhood/create.blade.php
@@ -8,32 +8,41 @@
                 إضافه مدينه
             </div>
             <div class="card-body">
-                <form method="POST" action="{{ url('/neighborhood/add') }}" aria-label="{{ __('Login') }}" enctype="multipart/form-data">
+                <form method="POST" action="{{ url('/neighborhood/add') }}" aria-label="{{ __('Login') }}"
+                      enctype="multipart/form-data">
                     @csrf
 
-                    <div class="form-inline row">
-                        <label for="title"
-                               class="col-sm-4 col-form-label text-md-right">أسم الحي</label>
+                    <div class="form-group row">
+                        <div class="col-md-offset-1 col-md-11 col-sm-12">
+                            <label for="name"
+                                   class="pull-left col-md-4 col-sm-12">أسم الحي</label>
 
-                        <div class="col-md-6">
-                            <input id="name" type="text"
-                                   class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name"
-                                   value="{{ old('name') }}" required autofocus>
+                            <div class="col-md-5">
+                                <input id="name" type="text"
+                                       class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name"
+                                       value="{{ old('name') }}" required autofocus>
 
-                            @if ($errors->has('name'))
-                                <span class="invalid-feedback" role="alert">
+                                @if ($errors->has('name'))
+                                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('name') }}</strong>
                                     </span>
-                            @endif
+                                @endif
+                            </div>
                         </div>
                     </div>
-                    <div class="form-inline row">
-                        <label class="pull-left col-sm-4" for="city_id">المدينه</label>
-                        <select id="type_id" class="form-control col-md-4" name="city_id" style="right: 2%;" required dir="rtl">
-                            @foreach($cities as $city)
-                                <option value="{{$city->id}}">{{$city->name}}</option>
-                            @endforeach
-                        </select>
+                    <div class="form-group row">
+                        <div class="col-md-offset-1 col-md-11 col-sm-12">
+                            <label class="col-md-4 col-sm-12" for="city_id">المدينه</label>
+                            <div class="col-md-5">
+                                <select id="city_id" class="form-control col-md-12 col-sm-12" name="city_id"
+                                        required
+                                        dir="rtl">
+                                    @foreach($cities as $city)
+                                        <option value="{{$city->id}}">{{$city->name}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
                     </div>
                     <div class="form-group row mb-0">
                         <div class="col-md-8 offset-md-4">
--- resources/views/News/addNews.blade.php
+++ resources/views/News/addNews.blade.php
@@ -13,51 +13,61 @@
                     @csrf
 
                     <div class="form-group row">
-                        <label for="title"
-                               class="col-sm-12 col-md-4 col-form-label text-md-right">عنوان الخبر</label>
+                        <div class="col-md-offset-1 col-md-11 col-sm-12">
+                            <label for="title"
+                                   class="pull-left col-md-4 col-sm-12">عنوان الخبر</label>
 
-                        <div class="col-md-6">
-                            <input id="title" type="title"
-                                   class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }}" name="title"
-                                   value="{{ old('title') }}" required autofocus>
+                            <div class="col-md-5">
+                                <input id="title" type="title"
+                                       class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }}" name="title"
+                                       value="{{ old('title') }}" required autofocus>
 
-                            @if ($errors->has('title'))
-                                <span class="invalid-feedback" role="alert">
+                                @if ($errors->has('title'))
+                                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('title') }}</strong>
                                     </span>
-                            @endif
+                                @endif
+                            </div>
                         </div>
                     </div>
                     <div class="form-group row ">
-                        <label for="text"
-                               class="col-sm-4 col-form-label text-md-right">نص الخبر</label>
+                        <div class="col-md-offset-1 col-md-11 col-sm-12">
+                            <label for="text"
+                                   class="pull-left col-md-4 col-sm-12">نص الخبر</label>
 
-                        <div class="col-md-6">
-                            <input id="text" type="text"
-                                   class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }}" name="text"
-                                   value="{{ old('text') }}" required autofocus>
+                            <div class="col-md-5">
+                                <input id="text" type="text"
+                                       class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }}" name="text"
+                                       value="{{ old('text') }}" required autofocus>
 
-                            @if ($errors->has('text'))
-                                <span class="invalid-feedback" role="alert">
+                                @if ($errors->has('text'))
+                                    <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('text') }}</strong>
                             </span>
-                            @endif
+                                @endif
+                            </div>
                         </div>
                     </div>
-                    <div class="form-inline row margin-btm">
-                        <label class="pull-left col-md-4 col-sm-12" for="type_id">النوع</label>
-                        <select id="type_id" class="form-control col-md-offset-2 col-md-5 col-sm-12" name="type_id"
-                                style="right: 2%;" required dir="rtl">
-                            @foreach($types as $type)
-                                <option value="{{$type->id}}">{{$type->name}}</option>
-                            @endforeach
-                        </select>
+                    <div class="form-group row margin-btm">
+                        <div class="col-md-offset-1 col-md-11 col-sm-12">
+                            <label class="col-md-4 col-sm-12" for="type_id">النوع</label>
+                            <div class="col-md-5">
+                                <select id="type_id" class="form-control col-md-12 col-sm-12" name="type_id"
+                                        style="right: 2%;" required dir="rtl">
+                                    @foreach($types as $type)
+                                        <option value="{{$type->id}}">{{$type->name}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                        </div>
                     </div>
                     <div class="form-group row">
-                        <label class="col-sm-12 col-md-4 col-form-label text-md-right">صورة الخبر</label>
-                        <div class="col-md-6">
+                        <div class="col-md-offset-1 col-md-11 col-sm-12">
+                        <label class="col-md-4 col-sm-12">صورة الخبر</label>
+                        <div class="col-md-5">
                             <input id="photo" type="file"
-                                   class="form-control {{ $errors->has('photo') ? ' is-invalid' : '' }}" name="photo">
+                                   class="form-control col-md-12 col-sm-12{{ $errors->has('photo') ? ' is-invalid' : '' }}" name="photo">
+                        </div>
                         </div>
                     </div>
                     <div class="form-group row mb-0">
