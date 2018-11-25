5b0cd1c3653c45f479827a33a411d67d516179e5
unknown <akame.pato@gmail.com>
2018-11-25 12:20:19 +0200

    change in user activity count

--- app/Model/Week.php
+++ app/Model/Week.php
@@ -8,28 +8,36 @@ use Illuminate\Database\Eloquent\Model;
 class Week extends Model
 {
     //
-    protected $fillable=["start_date" , "end_date" , "number_of_activities"];
-    public function activities(){
+    protected $fillable = ["start_date", "end_date", "number_of_activities"];
+
+    public function activities()
+    {
         return $this->hasMany('App\Model\Activity');
     }
-    public function userActivity(User $user){
+
+    public function userActivity(User $user)
+    {
         $userActivity = [];
-        $activities=$this->activities;
+        $activities = $this->activities;
 
-        foreach ($activities as $activity){
-            if ($activity->imam_id == $user->id){
+        foreach ($activities as $activity) {
+            if ($activity->imam_id == $user->id) {
 
-                $userActivity []= $activity;
+                $userActivity [] = $activity;
             }
         }
         //dd($userActivity);
         return $userActivity;
     }
-    public function userCount(User $user){
+
+    public function userCount(User $user)
+    {
         $count = 0;
-        $activities= $this->userActivity($user);
-        foreach ($activities as $activity){
-            $count +=$activity->type->point;
+        $activities = $this->userActivity($user);
+        foreach ($activities as $activity) {
+            if ($activity->type_id != null) {
+                $count += $activity->type->point;
+            }
         }
         return $count;
     }
