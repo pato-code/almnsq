5d045c9ce9308afd70a6234aa6af88c8162d2b26
unknown <akame.pato@gmail.com>
2018-11-29 10:49:36 +0200

    change name to id

--- config/app.php
+++ config/app.php
@@ -162,7 +162,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Ghanem\Rating\RatingServiceProvider::class,
-        FarhanWazir\GoogleMaps\GMapsServiceProvider::class,
+
 
     ],
 
@@ -212,7 +212,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'GMaps' => FarhanWazir\GoogleMaps\Facades\GMapsFacade::class,
+
 
     ],
 
